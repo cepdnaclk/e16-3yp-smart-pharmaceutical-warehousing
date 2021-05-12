3298a0cf9238f2830bbdc595a84ba65226fc143f
praveen dhananjaya <38405838+praveendhananjaya@users.noreply.github.com>
2021-05-12 12:12:20 +0530

    Add files via upload

---
+++ Hardware/AGV/code/mqtt2_working/mqtt2_working.ino
@@ -0,0 +1,120 @@
+/*
+
+const char* ssid = "SLT-Fiber"; // Enter your WiFi name
+const char* password = "home@12369"; // Enter WiFi password
+const char* mqttServer = "192.168.1.3";
+const int mqttPort = 37181;
+//const char* mqttUser = "otfxknod";
+//const char* mqttPassword = "nSuUc1dDLygF";
+*/
+ /*
+ * ESP8266 (Adafruit HUZZAH) Mosquitto MQTT Publish Example
+ * Thomas Varnish (https://github.com/tvarnish), (https://www.instructables.com/member/Tango172)
+ * Made as part of my MQTT Instructable - "How to use MQTT with the Raspberry Pi and ESP8266"
+ */
+#include <Bounce2.h> // Used for "debouncing" the pushbutton
+#include <ESP8266WiFi.h> // Enables the ESP8266 to connect to the local network (via WiFi)
+#include <PubSubClient.h> // Allows us to connect to, and publish to the MQTT broker
+
+const int ledPin = 0; // This code uses the built-in led for visual feedback that the button has been pressed
+const int buttonPin = 13; // Connect your button to pin #13
+
+// WiFi
+// Make sure to update this for your own WiFi network!
+const char* ssid = "SLT-Fiber";
+const char* wifi_password = "home@12369";
+
+// MQTT
+// Make sure to update this for your own MQTT Broker!
+const char* mqtt_server = "192.168.1.3";
+const char* mqtt_topic = "test";
+const char* mqtt_username = "praveen";
+const char* mqtt_password = "Pra350een";
+// The client id identifies the ESP8266 device. Think of it a bit like a hostname (Or just a name, like Greg).
+const char* clientID = "ESP12E";
+
+// Initialise the Pushbutton Bouncer object
+Bounce bouncer = Bounce();
+
+// Initialise the WiFi and MQTT Client objects
+WiFiClient wifiClient;
+PubSubClient client(mqtt_server, 1883, wifiClient); // 1883 is the listener port for the Broker
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+  // Switch the on-board LED off to start with
+  digitalWrite(ledPin, HIGH);
+
+  // Setup pushbutton Bouncer object
+  bouncer.attach(buttonPin);
+  bouncer.interval(5);
+
+  // Begin Serial on 115200
+  // Remember to choose the correct Baudrate on the Serial monitor!
+  // This is just for debugging purposes
+  Serial.begin(115200);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  // Connect to the WiFi
+  WiFi.begin(ssid, wifi_password);
+
+  // Wait until the connection has been confirmed before continuing
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Debugging - Output the IP Address of the ESP8266
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Connect to MQTT Broker
+  // client.connect returns a boolean value to let us know if the connection was successful.
+  // If the connection is failing, make sure you are using the correct MQTT Username and Password (Setup Earlier in the Instructable)
+  if (client.connect(clientID, mqtt_username, mqtt_password)) {
+    Serial.println("Connected to MQTT Broker!");
+  }
+  else {
+    Serial.println("Connection to MQTT Broker failed...");
+  }
+  
+}
+
+void loop() {
+
+  client.publish(mqtt_topic, "Button pressed!");
+  delay(500);
+  // Update button state
+  // This needs to be called so that the Bouncer object can check if the button has been pressed
+  bouncer.update();
+
+  if (bouncer.rose()) {
+    // Turn light on when button is pressed down
+    // (i.e. if the state of the button rose from 0 to 1 (not pressed to pressed))
+    digitalWrite(ledPin, LOW);
+
+    // PUBLISH to the MQTT Broker (topic = mqtt_topic, defined at the beginning)
+    // Here, "Button pressed!" is the Payload, but this could be changed to a sensor reading, for example.
+    if (client.publish(mqtt_topic, "Button pressed!")) {
+      Serial.println("Button pushed and message sent!");
+    }
+    // Again, client.publish will return a boolean value depending on whether it succeded or not.
+    // If the message failed to send, we will try again, as the connection may have broken.
+    else {
+      Serial.println("Message failed to send. Reconnecting to MQTT Broker and trying again");
+      client.connect(clientID, mqtt_username, mqtt_password);
+      delay(10); // This delay ensures that client.publish doesn't clash with the client.connect call
+      client.publish(mqtt_topic, "Button pressed!");
+    }
+  }
+  else if (bouncer.fell()) {
+    // Turn light off when button is released
+    // i.e. if state goes from high (1) to low (0) (pressed to not pressed)
+    digitalWrite(ledPin, HIGH);
+  }
+}
